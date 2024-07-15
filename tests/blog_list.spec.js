45cf3bd7de456ef3043cafa8d9204d4b2513ff7f
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-07-15 17:50:51 -0400

    Implemented login tests

--- tests/blog_list.spec.js
+++ tests/blog_list.spec.js
@@ -1,7 +1,17 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginWith } = require('./helper')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'Andrew Padgett',
+        username: 'adp10390',
+        password: 'weakpassword',
+      },
+    })
+
     await page.goto('http://localhost:5173')
   })
 
@@ -10,5 +20,19 @@ describe('Blog app', () => {
     await expect(page.getByText('username')).toBeVisible()
     await expect(page.getByText('password')).toBeVisible()
     await expect(page.getByText('login')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'adp10390', 'weakpassword')
+      await expect(page.getByText('Logged in as Andrew Padgett')).toBeVisible()
+      await expect(page.getByText('Andrew Padgett logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'adp10390', 'wrongpassword')
+      await expect(page.getByText('Wrong username or password')).toBeVisible()
+      await expect(page.getByText('Andrew Padgett logged in')).not.toBeVisible()
+    })
   })
 })
